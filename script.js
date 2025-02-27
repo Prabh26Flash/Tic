79f08755869ee726c501ebda3c19f1755c0d3dd6
Prabhjot Singh <154662377+Prabh26Flash@users.noreply.github.com>
2025-02-27 15:05:57 +0530

    Update script.js
    color functtionality added

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let currentPlayer = "X";
 let arr = Array(9).fill(null);
 // console.log(arr);
 
-winningCase = [
-  (0, 1, 2),
-  (3, 4, 5),
-  (6, 7, 8),
-  (0, 3, 6),
-  (1, 4, 7),
-  (2, 5, 8),
-  (0, 4, 8),
-  (2, 4, 6),
-];
+// winningCase = [
+//   (0, 1, 2),
+//   (3, 4, 5),
+//   (6, 7, 8),
+//   (0, 3, 6),
+//   (1, 4, 7),
+//   (2, 5, 8),
+//   (0, 4, 8),
+//   (2, 4, 6),
+// ];
 
 function checkWinner() {
   if (
